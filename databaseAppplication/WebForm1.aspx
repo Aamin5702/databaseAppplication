7ed4cf916290d9edf6c0f9c3647ef5d7fd876547
Aamin Shaikh <shaikhaamin5702@gmail.com>
2023-07-25 12:50:20 +0530

    Add project files.

---
+++ databaseAppplication/WebForm1.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="databaseAppplication.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Author&#39;s Details<br />
+            <br />
+            Name:
+            <asp:TextBox ID="txtName" runat="server" AutoPostBack="True"></asp:TextBox>
+            <br />
+            <br />
+            Phone No:
+            <asp:TextBox ID="txtPhone" runat="server" AutoPostBack="True"></asp:TextBox>
+            <br />
+            <br />
+            ID:
+            <asp:TextBox ID="txtID" runat="server" AutoPostBack="True"></asp:TextBox>
+            <br />
+            <br />
+            Addresss(City):
+            <asp:TextBox ID="txtAdd" runat="server" AutoPostBack="True"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnInsert" runat="server" OnClick="btnInsert_Click" Text="Insert Query" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete Query" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="btnView" runat="server" Text="View Record" OnClick="btnView_Click1"/>
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ databaseAppplication/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace databaseAppplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Aamin\source\repos\databaseAppplication\databaseAppplication\App_Data\Database1.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            string InsertQuery = "insert into Author values(@Author_Name, @Author_ID, @Author_Adrs, @Author_PhoneNo)";
+            SqlCommand cmd = new SqlCommand(InsertQuery, con);
+            cmd.Parameters.AddWithValue("@Author_Name", txtName.Text);
+            cmd.Parameters.AddWithValue("@Author_ID", txtID.Text);
+            cmd.Parameters.AddWithValue("@Author_PhoneNo", txtPhone.Text);
+            cmd.Parameters.AddWithValue("@Author_Adrs", txtAdd.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Label1.Text = "Record Inserted Successfully !!";
+            con.Close();
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            string DeleteQuery = "delete from Author where Author_ID=@Author_ID";
+            SqlCommand cmd = new SqlCommand(DeleteQuery, con);
+            cmd.Parameters.AddWithValue("@Author_ID", txtID.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            Label1.Text = "Record Deleted Successfuly.";
+            con.Close();
+
+        }
+
+        protected void btnView_Click1(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("Select * from Author", con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            GridView1.DataSource = reader;
+            GridView1.DataBind();
+
+        }
+    }
+}
